7a2afc74e015f4772dfca73ef4917374f8551a81
Iñaki Reta Sabarrós <jirsis@gmail.com>
2015-05-31 20:54:27 +0200

    Añadida solución de LSP

--- lsp/ElectronicDuck.java
+++ lsp/ElectronicDuck.java
@@ -1,8 +1,15 @@
 public class ElectronicDuck implements Duck{
   private boolean isTurnedOn = false;
   public void swim(){
-      if (!isTurnedOn)
-        return;
-      System.out.println("My electronic duck is swimming");
+      if (!isTurnedOn){
+        enableTurnedOn();
+        swim();
+      }else{
+        System.out.println("My electronic duck is swimming");
+      }
+  }
+
+  private void enableTurnedOn(){
+    isTurnedOn = true;
   }
 }
